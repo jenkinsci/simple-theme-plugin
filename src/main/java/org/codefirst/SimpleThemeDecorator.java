2079a89b00de7324069d6303964de2d9e3938764
mallowlabs <mallowlabs@gmail.com>
2011-04-09 11:52:22 +0900

    Added: alternative css feature

---
+++ src/main/java/org/codefirst/SimpleThemeDecorator.java
@@ -0,0 +1,31 @@
+package org.codefirst;
+
+import hudson.Extension;
+import hudson.model.PageDecorator;
+import net.sf.json.JSONObject;
+
+import org.kohsuke.stapler.StaplerRequest;
+
+@Extension
+public class SimpleThemeDecorator extends PageDecorator {
+	private String cssUrl;
+
+	public SimpleThemeDecorator() {
+		super(SimpleThemeDecorator.class);
+	}
+
+	@Override
+	public boolean configure(StaplerRequest req, JSONObject formData)
+			throws FormException {
+		cssUrl = formData.getString("cssUrl");
+		System.out.println("======DEBUG" + cssUrl);
+		save();
+		return super.configure(req, formData);
+	}
+
+	public String getCssUrl() {
+		System.out.println("======DEBUG(GET)" + cssUrl);
+		return cssUrl;
+	}
+
+}
