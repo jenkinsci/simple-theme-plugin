20ca41cb4dd65ba603d1a3236620184599f46c0c
Taiki Sugawara <buzz.taiki@gmail.com>
2016-03-11 05:01:11 +0900

    Add favicon support.
    Based on pull request #1

--- src/main/java/org/codefirst/SimpleThemeDecorator.java
+++ src/main/java/org/codefirst/SimpleThemeDecorator.java
@@ -3,34 +3,49 @@ package org.codefirst;
 import hudson.Extension;
 import hudson.model.PageDecorator;
 import net.sf.json.JSONObject;
-
 import org.kohsuke.stapler.StaplerRequest;
 
 @Extension
 public class SimpleThemeDecorator extends PageDecorator {
 	private String cssUrl;
 	private String jsUrl;
+	private String faviconUrl;
 
 	public SimpleThemeDecorator() {
-		super(SimpleThemeDecorator.class);
+		super();
 		load();
 	}
 
 	@Override
 	public boolean configure(StaplerRequest req, JSONObject formData)
 			throws FormException {
-		cssUrl = formData.getString("cssUrl");
-		jsUrl = formData.getString("jsUrl");
+		req.bindJSON(this, formData);
 		save();
-		return super.configure(req, formData);
+		return true;
+	}
+
+	public void setCssUrl(String cssUrl) {
+		this.cssUrl = cssUrl;
 	}
 
 	public String getCssUrl() {
 		return cssUrl;
 	}
 
+	public void setJsUrl(String jsUrl) {
+		this.jsUrl = jsUrl;
+	}
+
 	public String getJsUrl() {
 		return jsUrl;
+	}
+
+	public void setFaviconUrl(String faviconUrl) {
+		this.faviconUrl = faviconUrl;
+	}
+
+	public String getFaviconUrl() {
+		return faviconUrl;
 	}
 
 }
