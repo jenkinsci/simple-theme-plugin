481cbc24429540b10dc8fa58e68f5b1bc25ac24d
Tobias Gruetzmacher <tobias-git@23.gs>
2019-08-20 12:49:53 +0200

    Also test CasC export (#10)
    This should avoid problems like JENKINS-58590 in the future.

--- src/test/java/org/jenkinsci/plugins/simpletheme/ConfigurationAsCodeTest.java
+++ src/test/java/org/jenkinsci/plugins/simpletheme/ConfigurationAsCodeTest.java
@@ -1,28 +1,33 @@
 package org.jenkinsci.plugins.simpletheme;
 
+import static io.jenkins.plugins.casc.misc.Util.getUnclassifiedRoot;
+import static io.jenkins.plugins.casc.misc.Util.toStringFromYamlFile;
+import static io.jenkins.plugins.casc.misc.Util.toYamlString;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
-import io.jenkins.plugins.casc.ConfigurationAsCode;
-import io.jenkins.plugins.casc.ConfiguratorException;
+import io.jenkins.plugins.casc.ConfigurationContext;
+import io.jenkins.plugins.casc.ConfiguratorRegistry;
+import io.jenkins.plugins.casc.misc.ConfiguredWithCode;
+import io.jenkins.plugins.casc.misc.JenkinsConfiguredWithCodeRule;
+import io.jenkins.plugins.casc.model.CNode;
 import org.codefirst.SimpleThemeDecorator;
-import org.junit.Rule;
+import org.junit.ClassRule;
 import org.junit.Test;
-import org.jvnet.hudson.test.JenkinsRule;
 
 public class ConfigurationAsCodeTest {
 
-  @Rule public JenkinsRule j = new JenkinsRule();
+  @ClassRule
+  @ConfiguredWithCode("ConfigurationAsCode.yml")
+  public static JenkinsConfiguredWithCodeRule j = new JenkinsConfiguredWithCodeRule();
 
   @Test
-  public void testConfig() throws ConfiguratorException {
-    ConfigurationAsCode.get()
-        .configure(ConfigurationAsCodeTest.class.getResource("ConfigurationAsCode.yml").toString());
-
+  public void testConfig() {
     SimpleThemeDecorator decorator = j.jenkins.getDescriptorByType(SimpleThemeDecorator.class);
 
     assertNotNull(decorator.getElements());
@@ -34,5 +39,17 @@ public class ConfigurationAsCodeTest {
             hasProperty("text", containsString(".testcss")),
             hasProperty("url", containsString("test.js")),
             hasProperty("url", containsString("Favicon.ico"))));
+  }
+
+  @Test
+  public void testExport() throws Exception {
+    ConfigurationContext context = new ConfigurationContext(ConfiguratorRegistry.get());
+    CNode yourAttribute = getUnclassifiedRoot(context).get("simple-theme-plugin");
+
+    String exported = toYamlString(yourAttribute);
+
+    String expected = toStringFromYamlFile(this, "ConfigurationAsCodeExport.yml");
+
+    assertThat(exported, is(expected));
   }
 }
