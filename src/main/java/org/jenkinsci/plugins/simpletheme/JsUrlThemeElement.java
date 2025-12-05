0863ecf6ff7ddf95a359477f8c94ea17cfa27f79
Daniel Beck <daniel-beck@users.noreply.github.com>
2025-12-05 20:16:12 +0100

    Allow specific configured URLs in CSP

--- src/main/java/org/jenkinsci/plugins/simpletheme/JsUrlThemeElement.java
+++ src/main/java/org/jenkinsci/plugins/simpletheme/JsUrlThemeElement.java
@@ -6,11 +6,10 @@ import java.text.MessageFormat;
 import java.util.Set;
 import org.jenkinsci.Symbol;
 import org.kohsuke.stapler.DataBoundConstructor;
-import org.kohsuke.stapler.Stapler;
 
 public class JsUrlThemeElement extends UrlThemeElement {
 
-    private static final String JS_HTML = "<script type=\"text/javascript\" src=\"{0}\" nonce=\"{1}\"></script>";
+    private static final String JS_HTML = "<script type=\"text/javascript\" src=\"{0}\"></script>";
 
     @DataBoundConstructor
     public JsUrlThemeElement(String url) {
@@ -19,8 +18,7 @@ public class JsUrlThemeElement extends UrlThemeElement {
 
     @Override
     public void collectHeaderFragment(Set<String> fragments, boolean injectCss) {
-        fragments.add(MessageFormat.format(
-                JS_HTML, getUrl(), NonceContributor.getOrGenerateNonce(Stapler.getCurrentRequest2())));
+        fragments.add(MessageFormat.format(JS_HTML, getUrl()));
     }
 
     @Extension
